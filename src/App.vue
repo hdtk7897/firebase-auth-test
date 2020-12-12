4861613b25218861aa911fdb36c614452cb5179c
hdtk7897 <hdtk7897@gmail.com>
2020-12-13 00:43:44 +0900

    修正

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Test msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Test
   }
 }
 </script>
--- src/components/test.vue
+++ src/components/test.vue
@@ -23,7 +23,7 @@ export default {
   methods: {
     initFirebase: function() {
       if (!firebase.apps.length) {
-        firebase.initializeApp({
+        const result = firebase.initializeApp({
           apiKey: process.env.VUE_APP_API_KEY,
           authDomain: process.env.VUE_APP_AUTH_DOMAIN,
           databaseURL: process.env.VUE_APP_DATABASE_URL,
@@ -31,6 +31,7 @@ export default {
           storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
           messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
         });
+        console.log(result)
       }
       firebase.auth().onAuthStateChanged((user) => {
         if (user) {
